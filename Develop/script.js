c30121d76b9844d2c09e51a2b9bf53a0dc470f39
rick0048 <rick0048@algonquinlive.com>
2021-06-27 21:50:33 -0400

    Added comments and updated README

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,28 @@
+// Format a day object to the Full Day, Month, Day number then set the text of the current day <p> to that variable
 let currentDay = moment().format('dddd, MMMM Do')
 let currentDayEl = $('#currentDay').text(currentDay)
+
 let timeBlocks = $('.timeblocks')
 let hours = [9, 10, 11, 12, 13, 14, 15, 16, 17]
 
 function createBlocks() {
+    // empty the timeblocks, when you clear local storage timeblocks div is empty
     timeBlocks.empty()
 
+    // loop through the hours array and create a timeblock for each hour
     for(let i = 0; i < hours.length; i++) {
+        // Create a base day object starting at the beginning of the day and add the hours array items, therfore creating the proper times (9am - 5pm)
         let day = moment().startOf('day').add(hours[i], 'hours')
+        // Create a format for the day object (hour am/pm)
         let formattedDay = day.format('h A')
         let isBefore = day.isBefore(moment())
 
+        // Get all the local storage items with the index each time the array loops
         let toDoInput = localStorage.getItem(`todo-${i}`) || ""
+        // Create the timeblocks with unique ids and data-to-do
         timeBlocks.append('<div class="row"><p class="col-1 hour"> ' + formattedDay + '</p> <input type="text" id="input' + i + '" class="col-10 timeBlock" name="toDoInput" value="' + toDoInput + '"> <button data-to-do="' + i + '" class="col-1 saveBtn"><i class="far fa-save"></i></button> </div>')
 
+        // Check if the day object = the current hour or is before or after the current hour and change the color accordingly each time a timeblock is created
         if(day.isSame(moment().startOf('hour'))) {
             $(`#input${i}`).css('background-color', 'orangered')
         } else if(isBefore) {
@@ -24,22 +33,23 @@ function createBlocks() {
     }
 }
 
+// Call the creatBlocks function
 createBlocks()
 
+// Create onClick event on the save buttons in each timeblock
 $(document).on('click', '.saveBtn', function () {
+    // Get unique data-to-do value of each created button
     let toDoNumber = $(this).attr('data-to-do')
-    console.log(toDoNumber)
-
+    // Get unique id of each created input and the value of
     let toDoInput = $('#input' + toDoNumber).val()
-    console.log(toDoInput)
-
+    // Set the input value to local storage with unique reference
     localStorage.setItem(`todo-${toDoNumber}`, toDoInput)
-    console.log(localStorage)
 })
 
+// Create onClick event for clear local storage button
 $('#clear').on('click', function () {
+    // Clear the local storage
     localStorage.clear()
+    // Call function to create the fresh timeblocks
     createBlocks()
 })
-
-console.log(localStorage)
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Work Day Scheduler Starter Code
+# Charlie-Rick-Scheduler
+
+# Create todos for each hour in a normal workday
+# Local storage to keep track of todos in case browser is closed
+# Colors change based of past/present/future timeblocks
